af83023af72af91d07b9a7b38e55512be1ba39dc
Anton Levshunov <alevshunov@gmail.com>
2018-02-16 01:23:50 +0300

    Increased refresh delay.

--- service/looter/src/db/ShopStorage.ts
+++ service/looter/src/db/ShopStorage.ts
@@ -46,7 +46,7 @@ export class ShopStorage implements IShopProvider {
             const conn = new MyConnection(this._dbConnection);
             await conn.open();
             const result = await conn.query(
-                "select * from shops where active and (last_fetch < date_add(now(), interval -2 hour) or last_fetch is null) order by last_fetch asc, id desc limit 1",
+                "select * from shops where active and (last_fetch < date_add(now(), interval -6 hour) or last_fetch is null) order by last_fetch asc, id desc limit 1",
                 );
 
             conn.close();
