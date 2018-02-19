935c5534d8e191858d630f1af040e4ca0c815267
Anton Levshunov <alevshunov@objectstyle.com>
2018-02-19 15:45:07 +0300

    Handle pm messages.

--- service/looter/src/freero/hub/FreeRoIrcHub.ts
+++ service/looter/src/freero/hub/FreeRoIrcHub.ts
@@ -7,7 +7,8 @@ import {IIrcConnection} from "./IIrcConnection";
 export class FreeRoIrcHub implements IEventProvider<FreeRoEventArgs> {
     private _irc: IrcClient | IIrcConnection;
 
-    private IRC_MESSAGE_EVENT = 'message';
+    private IRC_MESSAGE_DIRECT_EVENT = 'pm';
+    private IRC_MESSAGE_EVENT = 'message#freero';
     private IRC_ERROR_EVENT = 'error';
 
     private _onEvent = new EventDispatcher<FreeRoIrcHub, FreeRoEventArgs>();
@@ -18,11 +19,24 @@ export class FreeRoIrcHub implements IEventProvider<FreeRoEventArgs> {
     }
 
     private init() {
+        this._irc.addListener(this.IRC_MESSAGE_DIRECT_EVENT, this.ircDirectMessageHandler.bind(this));
         this._irc.addListener(this.IRC_MESSAGE_EVENT, this.ircMessageHandler.bind(this));
         this._irc.addListener(this.IRC_ERROR_EVENT, this.ircErrorHandler.bind(this))
     }
 
-    private ircMessageHandler(from: string, to: string, message: string) {
+    private ircDirectMessageHandler(from: string, message: string, extra: any) {
+        console.log('MSG >> ', from, '>', message);
+        console.log('MSG >> ', JSON.stringify(extra));
+
+        const freeRoEvent = new FreeRoEventArgs('FreeRO-PM', message, new Date());
+
+        this._onEvent.dispatch(this, freeRoEvent);
+    }
+
+    private ircMessageHandler(from: string, message: string, extra: any) {
+        console.log('MSG >> ', from, '>', message);
+        console.log('MSG >> ', JSON.stringify(extra));
+
         const freeRoEvent = new FreeRoEventArgs(from, message, new Date());
 
         this._onEvent.dispatch(this, freeRoEvent);
