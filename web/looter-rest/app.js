949f9b99f8478c5d8483d2930a8aec061b4eb724
Anton Levshunov <alevshunov@objectstyle.com>
2018-02-20 18:49:16 +0300

    Endpoint updated.

--- web/looter-rest/app.js
+++ web/looter-rest/app.js
@@ -4,9 +4,7 @@ var express    = require('express');
 var app        = express();
 var router = express.Router();
 
-router.get('/cards', function(req, res, next){
-    console.log('GET /rest/cards', req.headers['x-forwarded-for'] || req.connection.remoteAddress);
-
+function getConnection(cb) {
     var connection = mysql.createConnection({
         host: process.env.LOOTER_DB_HOST,
         user: process.env.LOOTER_DB_USER,
@@ -14,14 +12,20 @@ router.get('/cards', function(req, res, next){
         database: process.env.LOOTER_DB_DBNAME
     });
 
-    const term = req.query.term ? `%${req.query.term || ''}%` : '%';
-
     connection.connect((e) => {
-        if(e) {
+        if (e) {
             console.log(e);
             throw e;
         }
 
+        cb(connection);
+    });
+}
+
+router.get('/cards', function(req, res, next){
+    const term = req.query.term ? `%${req.query.term || ''}%` : '%';
+
+    getConnection(connection => {
         connection.query("select owner, card, date from card_drop where owner like ? or card like ? order by date desc limit 100",
             [term, term],
             (err, result) => {
@@ -33,69 +37,46 @@ router.get('/cards', function(req, res, next){
     });
 });
 
-router.get('/shops', function(req, res, next){
-    console.log('GET /rest/shops', req.headers['x-forwarded-for'] || req.connection.remoteAddress);
-
-    var connection = mysql.createConnection({
-        host: process.env.LOOTER_DB_HOST,
-        user: process.env.LOOTER_DB_USER,
-        password: process.env.LOOTER_DB_PASSWORD,
-        database: process.env.LOOTER_DB_DBNAME
-    });
-
-    const term = req.query.term ? `%${req.query.term || ''}%` : '%';
-
-    connection.connect((e) => {
-        if(e) {
-            console.log(e);
-            throw e;
-        }
-
-        connection.query(`
-            select t1.name, t1.c as count1, t2.c as count2, t1.c - ifnull(t2.c,0) as delta, t1.minp, t1.maxp
-            from
-            (
-                select si.name, sum(count) c, max(si.price) maxp, min(si.price) minp
-                from shop_items si inner join shops s on s.id = si.shop_id
-                where si.fetch_index = 1
-                group by name
-            ) t1
-            left join
-            (
-                select si.name, sum(count) c
-                from shop_items si inner join shops s on s.id = si.shop_id
-                where si.fetch_index = s.fetch_count 
-                group by si.name
-            ) t2 on t1.name = t2.name
-            where t1.name like ?
-        `,
-            [term],
-            (err, result) => {
-                if (err) { console.log(err); throw err; }
-                res.json(result);
-                connection.destroy();
-            }
-        );
-    });
-});
+//
+// router.get('/shops', function(req, res, next){
+//     console.log('GET /rest/shops', req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+//
+//     const term = req.query.term ? `%${req.query.term || ''}%` : '%';
+//
+//     getConnection(connection => {
+//
+//         connection.query(`
+//             select t1.name, t1.c as count1, t2.c as count2, t1.c - ifnull(t2.c,0) as delta, t1.minp, t1.maxp
+//             from
+//             (
+//                 select si.name, sum(count) c, max(si.price) maxp, min(si.price) minp
+//                 from shop_items si inner join shops s on s.id = si.shop_id
+//                 where si.fetch_index = 1
+//                 group by name
+//             ) t1
+//             left join
+//             (
+//                 select si.name, sum(count) c
+//                 from shop_items si inner join shops s on s.id = si.shop_id
+//                 where si.fetch_index = s.fetch_count
+//                 group by si.name
+//             ) t2 on t1.name = t2.name
+//             where t1.name like ?
+//         `,
+//             [term],
+//             (err, result) => {
+//                 if (err) { console.log(err); throw err; }
+//                 res.json(result);
+//                 connection.destroy();
+//             }
+//         );
+//     });
+// });
 
 router.get('/shops/active', function(req, res, next){
-    console.log('GET /rest/shops/active', req.headers['x-forwarded-for'] || req.connection.remoteAddress);
-
-    var connection = mysql.createConnection({
-        host: process.env.LOOTER_DB_HOST,
-        user: process.env.LOOTER_DB_USER,
-        password: process.env.LOOTER_DB_PASSWORD,
-        database: process.env.LOOTER_DB_DBNAME
-    });
-
     const term = req.query.term ? `%${req.query.term || ''}%` : '%';
 
-    connection.connect((e) => {
-        if(e) {
-            console.log(e);
-            throw e;
-        }
+    getConnection(connection => {
 
         connection.query(`
             select si.name, sum(si.count) as count, min(si.price) as min, max(si.price) as max
@@ -110,6 +91,68 @@ router.get('/shops/active', function(req, res, next){
                 connection.destroy();
             }
         );
+    });
+});
+
+router.get('/shops/all', function(req, res, next){
+    const id = req.params.id;
+
+    getConnection(connection => {
+        connection.query(`
+            select *
+            from shops s
+            where s.active and s.fetch_count > 0
+            order by s.date desc
+            limit 100
+        `,
+            [id],
+            (err, result) => {
+                if (err) { console.log(err); throw err; }
+                res.json(result);
+                connection.destroy();
+            }
+        );
+    });
+});
+
+router.get('/shop/:id', function(req, res, next){
+    const id = req.params.id;
+
+    getConnection(connection => {
+        connection.query(
+            `
+                select *
+                from shops s
+                where s.id = ?
+            `,
+            [id],
+            (err, result) => {
+
+                if (err) { console.log(err); throw err; }
+
+                var shop = result[0];
+
+                connection.query(
+                    `
+                        select si.*
+                        from shops s inner join shop_items si on s.id = si.shop_id and s.fetch_count = si.fetch_index
+                        where s.id = ?
+                        order by si.id
+                    `,
+                    [id],
+                    (err, result) => {
+                        if (err) { console.log(err); throw err; }
+
+                        shop.items = result;
+                        res.json(shop);
+
+                        connection.destroy();
+                    }
+                );
+
+            }
+        );
+
     });
 });
 
