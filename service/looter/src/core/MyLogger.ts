ec1760187b05f9fadc8332bfd10f9dab1884e1e2
Anton Levshunov <alevshunov@objectstyle.com>
2018-02-26 18:26:31 +0300

    logger adjusted

--- service/looter/src/core/MyLogger.ts
+++ service/looter/src/core/MyLogger.ts
@@ -1,6 +1,6 @@
 export class MyLogger {
     static log(...args: any[]) {
-        console.log(`[${new Date()}]:`, args.join('; '));
+        console.log(`[${new Date()}]:`, args);
     }
 
     public log(...args: any[]) {
