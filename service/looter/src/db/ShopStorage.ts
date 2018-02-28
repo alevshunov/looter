62cdfbe6d3d7eaf83cde28e7a8a53b78a9b4ed23
Anton Levshunov <alevshunov@objectstyle.com>
2018-02-28 18:41:10 +0300

    cleanup

--- service/looter/src/db/ShopStorage.ts
+++ service/looter/src/db/ShopStorage.ts
@@ -50,7 +50,6 @@ export class ShopStorage implements IShopProvider {
                     from shops 
                     where active 
                     and (last_fetch < date_add(now(), interval -4 hour) or last_fetch is null) 
-                    -- and date < date_add(now(), interval -1 minute) 
                     order by last_fetch asc, date desc, id desc 
                     limit 1
                 `,
