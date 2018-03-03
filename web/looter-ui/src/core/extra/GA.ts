23dce09e58175a71f7c59e17d5eb56f6d7e648bf
Anton Levshunov <alevshunov@objectstyle.com>
2018-03-03 15:11:06 +0300

    GA adjusted.

--- web/looter-ui/src/core/extra/GA.ts
+++ web/looter-ui/src/core/extra/GA.ts
@@ -1,11 +1,13 @@
 export default function () {
     const last = window['last'] || (location.pathname + location.search);
-    if (window['ga'] && typeof window['ga'] === 'function' && last !== location.pathname + location.search) {
-        try {
-            window['ga']('set', 'page', location.pathname + location.search);
-            window['ga']('send', 'pageview');
-        } catch (e) {
-            console.warn(e);
+    if (window['ga'] && typeof window['ga'] === 'function') {
+        if (last !== location.pathname + location.search) {
+            try {
+                window['ga']('set', 'page', location.pathname + location.search);
+                window['ga']('send', 'pageview');
+            } catch (e) {
+                console.warn(e);
+            }
         }
     } else {
         console.warn('window.ga is undefined');
