5554003afb0762cd5a422029a50a4fab14cd63b8
Anton Levshunov <alevshunov@objectstyle.com>
2018-03-03 15:09:12 +0300

    GA adjusted.

--- web/looter-ui/src/core/extra/GA.ts
+++ web/looter-ui/src/core/extra/GA.ts
@@ -1,12 +1,15 @@
 export default function () {
-    if (window['ga'] && typeof window['ga'] === 'function') {
+    const last = window['last'] || (location.pathname + location.search);
+    if (window['ga'] && typeof window['ga'] === 'function' && last !== location.pathname + location.search) {
         try {
             window['ga']('set', 'page', location.pathname + location.search);
-            window['ga']('send', 'pageview', location.pathname + location.search);
+            window['ga']('send', 'pageview');
         } catch (e) {
             console.warn(e);
         }
     } else {
         console.warn('window.ga is undefined');
     }
+
+    window['last'] = location.pathname + location.search;
 }
