3cfc6e3b0df06581120340ad41248a52f3716873
Anton Levshunov <alevshunov@objectstyle.com>
2018-10-26 16:53:53 +0300

    Halloween ;)

--- web/looter-ui/src/index.tsx
+++ web/looter-ui/src/index.tsx
@@ -12,3 +12,35 @@ ReactDOM.render(
     document.getElementById('root') as HTMLElement
 );
 x.unregister();
+
+const items = [
+    '1.jpg',
+    '2.png',
+    '3.png',
+    '4.png',
+    '5.png',
+    '6.jpg'
+];
+
+const area = document.getElementById('img');
+
+function setBgImg() {
+    if (!area) {
+        return;
+    }
+
+    if (!(new Date().getHours() >= 18 || new Date().getHours() <= 5)) {
+        area.setAttribute('style', 'background-image: none;');
+    } else {
+        let item = items[Math.floor(Math.random() * items.length)];
+        area.setAttribute('style', 'background-image: url(/img/' + item + '); animation: pulse 60s infinite;');
+    }
+}
+
+if (area) {
+    setBgImg();
+
+    setInterval(() => {
+        setBgImg();
+    }, 60000);
+}
