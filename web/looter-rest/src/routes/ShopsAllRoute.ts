352338e589035d2c094510dfb9bb80d99c7ce85d
Anton Levshunov <alevshunov@objectstyle.com>
2019-02-05 13:18:29 +0300

    Shop search extended: item name and id are included.

--- web/looter-rest/src/routes/ShopsAllRoute.ts
+++ web/looter-rest/src/routes/ShopsAllRoute.ts
@@ -10,17 +10,22 @@ class ShopsAllRoute implements IRouteWithConnection {
         const args = extractTermDetails(req.query.term);
         const data = await connection.query(`
             select s.id, s.name, s.location, s.owner, s.date, s.type, group_concat(distinct(i.id) order by si.id separator ',') items
-            from shops s
+            from (
+                   select s.*
+                   from
+                      shops s
+                      inner join shop_items si on si.shop_id = s.id and si.fetch_index = s.fetch_count
+                   where s.active = 1 and s.fetch_count > 0
+                   and (s.name like ? or s.owner like ? or s.location like ? or si.name like ? or si.id like ?)
+                   and s.type like ?
+                 ) s
               inner join shop_items si on si.shop_id = s.id and si.fetch_index = s.fetch_count
               left join item_db_2 i on i.name = si.name
-            where s.active = 1 and s.fetch_count > 0
-            and (s.name like ? or s.owner like ? or s.location like ?)
-            and s.type like ?
               group by s.id
             order by s.date desc
             limit 100
         `,
-            args.term, args.term, args.term, args.direction,
+            args.term, args.term, args.term, args.term, args.term, args.direction,
         );
         return data;
     }
